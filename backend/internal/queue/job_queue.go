6441715ab3e191291b000b23c7126c926d2c7e98
Abhishek Dadwal <dadwalabhishek10@gmail.com>
2026-01-07 22:43:33 +0530

    new files

--- backend/internal/queue/job_queue.go
+++ backend/internal/queue/job_queue.go
@@ -22,7 +22,7 @@ type Job struct {
 
 type JobQueue struct {
 	executor executor.Executor
-	jobs     chan Job // these are the lists of jobs which will get added here core feature anyhow
+	jobs     chan Job // these are the lists of jobs which will get added here core feature anyhow wed
 	workers  int
 }
 
