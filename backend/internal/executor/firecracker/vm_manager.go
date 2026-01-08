eb2a3f9b6f3a582565e8bea48475e139138d701e
Abhishek Dadwal <dadwalabhishek10@gmail.com>
2026-01-08 21:08:58 +0530

    Firecracker config added

--- backend/internal/executor/firecracker/vm_manager.go
+++ backend/internal/executor/firecracker/vm_manager.go
@@ -33,6 +33,29 @@ type MicroVM struct {
 	CreatedAt  time.Time
 }
 
+type BootSource struct {
+	KernelImagePath string `json:"kernel_image_path"`
+	BootArgs        string `json:"boot_args"`
+}
+
+type Drive struct {
+	DriveId      string `json:"drive_id"`
+	PathOnHost   string `json:"path_on_host"`
+	IsRootDevice bool   `json:"is_root_device"`
+	IsReadOnly   bool   `json:"is_read_only"`
+}
+
+type MachineConfig struct {
+	VCPUCount  int `json:"vcpu_count"`
+	MemSizeMib int `json:"mem_size_mib"`
+}
+
+type FirecrackerConfig struct {
+	BootSource    BootSource    `json:"boot-source"`
+	Drives        []Drive       `json:"drives"`
+	MachineConfig MachineConfig `json:"machine-config"`
+}
+
 type VMManager interface {
 	Create(ctx context.Context, cfg VMConfig) (*MicroVM, error)
 	Boot(ctx context.Context, vmID string) error
