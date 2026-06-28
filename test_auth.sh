a35f9a9987d7bb034f55da4010ce7519c953711b
Abhishek Dadwal <dadwalabhishek10@gmail.com>
2026-06-29 01:38:21 +0530

    chore: remove stale docs/scripts/test files, ignore Python artifacts

--- test_auth.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -s -X POST http://localhost:8080/execute \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer comp_key_test1234567890abcdef12345678" \
-  -d '{"code":"print(1+1)","language":"python"}'
